1a82c018ee0f61929f39b8ca8488c31a117a222b
Wairimu2018 <wairimukuria.wk@gmail.com>
2022-09-16 20:26:41 +0300

    migrate

--- db/migrate/20220915134805_create_foods_table.rb
+++ db/migrate/20220915134805_create_foods_table.rb
@@ -1,8 +1,8 @@
-class CreateFoodsTable < ActiveRecord::Migration[6.1]
-  def change
-    create_table :foods do |t|
-      t.string :text
-      t.integer :meal_id
-    end
-  end
-end
+# class CreateFoodsTable < ActiveRecord::Migration[6.1]
+#   def change
+#     create_table :foods do |t|
+#       t.string :text
+#       t.integer :meal_id
+#     end
+#   end
+# end
